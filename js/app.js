4fa5012c1fced6a329c8d62f4f242f789814a8f1
Radek <r.ciepichal@gmail.com>
2022-01-02 22:46:07 +0100

    Contact only

--- js/app.js
+++ js/app.js
@@ -1,38 +1,8 @@
-(function () {
-  "use strict";
+let navBar = document.getElementById("navigation");
+let toggler = document.getElementById("toggler");
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
-  };
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach((e) => e.addEventListener(type, listener));
-      } else {
-        selectEl.addEventListener(type, listener);
-      }
-    }
-  };
-
-  /**
-   * Mobile nav toggle
-   */
-  on("click", ".toggler__icon", function (e) {
-    select("body").classList.toggle("mobile-nav-active");
-    this.classList.toggle("fa-list");
-    this.classList.toggle("fa-times");
-  });
-})();
+function toggleFunction() {
+  navBar.classList.toggle("mobile-nav-active");
+  toggler.classList.toggle("fa-list");
+  toggler.classList.toggle("fa-times");
+}
---
+++ js/archiveApp.js
@@ -0,0 +1,38 @@
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".toggler__icon", function (e) {
+    select("body").classList.toggle("mobile-nav-active");
+    this.classList.toggle("fa-list");
+    this.classList.toggle("fa-times");
+  });
+})();
