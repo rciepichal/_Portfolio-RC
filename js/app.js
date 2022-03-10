b3181955192d5237587750b82174a2f7bc3a1bf5
Radek <r.ciepichal@gmail.com>
2022-03-10 18:28:21 +0100

    Visual Fixes

--- js/app.js
+++ js/app.js
@@ -1,41 +1,21 @@
 const navBar = document.getElementById('navigation');
 const toggler = document.getElementById('toggler');
+const overlay = document.getElementById('overlay');
+const filterItems = document.querySelectorAll('.work__list-element');
+const filterButtons = document.querySelector('#filter-btns').children;
 
-// const filterItem = document.querySelector('.work__list').children;
-
+// NAV TOGGLER
 toggler.addEventListener('click', () => toggleFunction());
+overlay.addEventListener('click', () => toggleFunction());
 
 function toggleFunction() {
   navBar.classList.toggle('mobile-nav-active');
+  overlay.classList.toggle('ovActive');
   toggler.classList.toggle('fa-list');
   toggler.classList.toggle('fa-times');
 }
 
-// for (let i = 0; i < filterButtons.length; i++) {
-//   filterButtons[i].addEventListener('click', () => {
-//     for (let j = 0; j < filterButtons.length; j++) {
-//       filterButtons[j].classList.remove('active');
-//     }
-//     filterButtons[i].classList.add('active');
-//     const target = filterButtons[i].getAttribute('data-target');
-//     for (let k = 0; k < filterItem.length; k++) {
-//       filterItem[k].style.display = 'none';
-//       filterItem[k].classList.add('delete');
-//       if (target == filterItem[k].getAttribute('data-id')) {
-//         filterItem[k].style.display = 'block';
-//       }
-//       if (target == 'all') {
-//         filterItem[k].style.display = 'block';
-//       }
-//     }
-//   });
-// }
-
-// FILTER
-
-const filterItems = document.querySelectorAll('.work__list-element');
-const filterButtons = document.querySelector('#filter-btns').children;
-
+//FILTER
 const showAll = () => {
   for (const element of filterItems) {
     element.classList.add('show');
@@ -47,29 +27,13 @@ const buttonActive = () => {
   for (let i = 0; i < filterButtons.length; i++) {
     filterButtons[i].addEventListener('click', () => {
       for (let j = 0; j < filterButtons.length; j++) {
-        filterButtons[j].classList.remove('active');
+        filterButtons[j].classList.remove('active-filter');
       }
-      filterButtons[i].classList.add('active');
+      filterButtons[i].classList.add('active-filter');
     });
   }
 };
 buttonActive();
-
-//Poprawne sortowanie Funkcja:
-// const filteringFunction = (category) => {
-//   for (const el of filterItems) {
-//     el.classList.remove('show');
-//     el.classList.add('hidden');
-//     if ('all' == category) {
-//       el.classList.remove('hidden');
-//       el.classList.add('show');
-//     }
-//     if (el.getAttribute('data-id') == category) {
-//       el.classList.remove('hidden');
-//       el.classList.add('show');
-//     }
-//   }
-// };
 
 const filteringFunction = (category) => {
   for (const el of filterItems) {
